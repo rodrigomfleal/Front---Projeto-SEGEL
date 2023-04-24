a35927c96b11c4aa12871a0d8acdf21fe92b5398
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-04-24 19:00:55 -0300

    feat: checking user_type to show a different menu for different users and color update to fit figmas prototype

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar color="red" class="flex-grow-0" app light>
+    <v-app-bar color="primary" class="flex-grow-0" app light>
       <v-app-bar-title></v-app-bar-title>
     </v-app-bar>
-    <template v-if="!authenticated">
+    <template v-if="!$auth.loggedIn">
         <LoginComponent />
     </template>
-    <v-navigation-drawer app permanent v-if="$auth.loggedIn">
+    <v-navigation-drawer app color="primary" permanent v-if="$auth.loggedIn">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6"> SACCE </v-list-item-title>
@@ -15,7 +15,12 @@
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item 
+          v-for="item in menuItems" 
+          :key="item.title" 
+          link 
+          :to="item.path"
+        >
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -37,13 +42,25 @@ export default {
   components: {
     LoginComponent
   },
-  data: () => ({
-    items: [
-      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-      { title: 'Account', icon: 'mdi-account-box' },
-      { title: 'Settings', icon: 'mdi-cog' },
-    ],
-    authenticated: false
-  }),
+  computed: {
+    menuItems() {
+      const userType = this.$auth.user.user_type; // Assuming user_type is a field in your user model
+      if (userType === 0) { // Manager user
+        return [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '/dashboard' },
+          { title: 'Minha Conta', icon: 'mdi-account-box', path: '/account' },
+          { title: 'Configurações', icon: 'mdi-cog', path: '/settings' },
+          { title: 'Criar Área', icon: 'mdi-account-plus', path: '/area'},
+        ];
+      } else { // Regular user
+        return [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '/dashboard' },
+          { title: 'Minha Conta', icon: 'mdi-account-box', path: '/account' },
+          { title: 'Configurações', icon: 'mdi-cog', path: '/settings' },
+          { title: 'Reservas', icon: 'mdi-calendar', path: '/reservations' },
+        ];
+      }
+    }
+  }
 };
 </script>
